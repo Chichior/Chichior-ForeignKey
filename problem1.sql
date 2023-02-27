a6b64d5b47c3626716e0a7f532530de144cde2a1
Chichior <katiehakala@icloud.com>
2023-02-27 21:19:23 +0000

    ForeignKeyComplete

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,2 @@
-CREATE TABLE post( id SERIAL PRIMARY KEY, post VARCHAR(255), user_fk INT, FOREIGN KEY(user_fk) REFERENCES site_user(id));
+CREATE TABLE post( id SERIAL PRIMARY KEY, post VARCHAR(255), user_fk INT, FOREIGN KEY(user_fk) REFERENCES site_user(id)); 
+--
